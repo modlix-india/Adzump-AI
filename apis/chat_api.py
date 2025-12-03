31a09f1a8b9e5cfeff468ecb8fde5520f424f684
pavankumar-modlix <vasala.pavan@modlix.com>
2025-12-03 12:42:00 +0530

    Added account auto selection when user has single accouns -Added seperate endpoint for getting the session data

--- apis/chat_api.py
+++ apis/chat_api.py
@@ -9,7 +9,7 @@ async def start_session():
     return await chat_service.start_session()
 
 
-@router.post("/{session_id}")
+@router.post("/{session_id}") 
 async def chat(session_id: str, message: str, clientCode: str = Query(...)):
     return await chat_service.process_chat(session_id, message, clientCode)
 
@@ -17,3 +17,7 @@ async def chat(session_id: str, message: str, clientCode: str = Query(...)):
 @router.post("/end-session/{session_id}")
 async def end_session(session_id: str):
     return await chat_service.end_session(session_id)
+
+@router.get("/session/{session_id}")
+async def get_session(session_id: str):
+    return await chat_service.get_session_data(session_id)
